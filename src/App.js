a685394f9476b2db181521f6f1246fdea8b56ced
Romilj012 <romilj012@gmail.com>
2024-11-19 00:33:33 -0600

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import TemplateList from './components/TemplateList';
+import TemplateDetails from './components/TemplateDetails';
 import theme from './theme';
 
 function App() {
@@ -26,25 +29,50 @@ function App() {
     setUser(null);
   };
 
-  if (!user) {
-    return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {isLoginView ? (
-          <Login onLogin={handleLogin} onSwitchToSignup={() => setIsLoginView(false)} />
-        ) : (
-          <Signup onSignup={handleSignup} onSwitchToLogin={() => setIsLoginView(true)} />
-        )}
-      </ThemeProvider>
-    );
-  }
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Dashboard user={user} onLogout={handleLogout} />
+      <Router>
+        <Routes>
+          {!user ? (
+            <>
+              <Route 
+                path="/login" 
+                element={
+                  <Login 
+                    onLogin={handleLogin} 
+                    onSwitchToSignup={() => setIsLoginView(false)} 
+                  />
+                } 
+              />
+              <Route 
+                path="/signup" 
+                element={
+                  <Signup 
+                    onSignup={handleSignup} 
+                    onSwitchToLogin={() => setIsLoginView(true)} 
+                  />
+                } 
+              />
+              <Route path="*" element={<Navigate to="/login" replace />} />
+            </>
+          ) : (
+            <>
+              <Route 
+                path="/" 
+                element={<Dashboard user={user} onLogout={handleLogout} />} 
+              />
+              <Route path="/templates" element={<TemplateList />} />
+              <Route path="/templates/:id" element={<TemplateDetails user={user} onLogout={handleLogout} />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </>
+          )}
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
 
 export default App;
+
+
